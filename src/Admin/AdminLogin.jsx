1d854ec35ba479a5ebb4e00398df3418b6ca8cab
Grishma-M <maithaniagrishma@gmail.com>
2024-06-02 16:03:12 +0530

    Update @babel/plugin-proposal-private-property-in-object to a valid version

--- src/Admin/AdminLogin.jsx
+++ src/Admin/AdminLogin.jsx
@@ -17,7 +17,7 @@ else{
         username:username,
         password:password
     }
-    axios.post("http://localhost:5000/api/admin/adminLogin",bodyjson).then((res)=>{
+    axios.post("https://internarea-backend-qfpt.onrender.com/api/admin/adminLogin",bodyjson).then((res)=>{
         console.log(res,"data is send")
         alert("success")
         navigate("/adminpanel")
